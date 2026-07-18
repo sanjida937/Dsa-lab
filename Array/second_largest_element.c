0078c0ce38b5bf5285e5af4cee527665e1346c00
sanjida937 <sanjidaakterasha93@gmail.com>
2026-07-18 20:24:35 +0600

    Implement second largest element finder in C
    This program reads an array of integers and finds the second largest element.

---
+++ Array/second_largest_element.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    int largest=INT_MIN;
+    int second=INT_MIN;
+
+    for(int i=0;i<n;i++) {
+
+        if(arr[i]>largest) {
+            second=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]>second && arr[i]!=largest) {
+            second=arr[i];
+        }
+    }
+
+    printf("%d",second);
+
+}
